3b20d76e94a6f1e6918b249913a04d7f6c6985df
unknown <dirie>
2023-06-12 11:53:53 +0300

    quick sorting and merge sorting algorithm added.

---
+++ cpp/chapter9/quickSort.cpp
@@ -0,0 +1,78 @@
+// C++ code to implement quicksort
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// This function takes last element as pivot,
+// places the pivot element at its correct position
+// in sorted array, and places all smaller to left
+// of pivot and all greater elements to right of pivot
+int partition(int arr[], int low, int high)
+{
+	// Choosing the pivot
+	int pivot = arr[high];
+
+	// Index of smaller element and indicates
+	// the right position of pivot found so far
+	int i = (low - 1);
+
+	for (int j = low; j <= high - 1; j++) {
+
+		// If current element is smaller than the pivot
+		if (arr[j] < pivot) {
+
+			// Increment index of smaller element
+			i++;
+			swap(arr[i], arr[j]);
+		}
+	}
+	swap(arr[i + 1], arr[high]);
+	return (i + 1);
+}
+
+// The main function that implements QuickSort
+// arr[] --> Array to be sorted,
+// low --> Starting index,
+// high --> Ending index
+void quickSort(int arr[], int low, int high)
+{
+	if (low < high) {
+
+		// pi is partitioning index, arr[p]
+		// is now at right place
+		int pi = partition(arr, low, high);
+
+		// Separately sort elements before
+		// partition and after partition
+		quickSort(arr, low, pi - 1);
+		quickSort(arr, pi + 1, high);
+	}
+}
+void display(int *array, int size) {
+   for(int i = 0; i<size; i++)
+      cout << array[i] << " ";
+   cout << endl;
+}
+// Driver Code
+int main()
+{
+	//int arr[] = { 10, 7, 8, 9, 1, 5 };
+	//int N = sizeof(arr) / sizeof(arr[0]);
+   int N;
+   cout << "Enter the number of elements: ";
+   cin >> N;
+   int arr[N];     //create an array with given number of elements
+   cout << "Enter elements:" << endl;
+   for(int i = 0; i<N; i++) {
+      cin >> arr[i];
+   }
+   cout << "Array before Sorting: ";
+   display(arr, N);
+
+	// Function call
+	quickSort(arr, 0, N - 1);
+	cout << "Array after Sorting: ";
+    display(arr, N);
+	
+	return 0;
+}
